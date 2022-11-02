d3055a52ef917593f6843d511305730697be3e2e
Jang Gyeoungguk <97073355+rnr9928@users.noreply.github.com>
2022-11-02 20:36:40 +0900

    Add files via upload

--- weather/App.js
+++ weather/App.js
@@ -1,95 +1,143 @@
-import * as Location from "expo-location"
-import React, { useState,useEffect } from 'react';
-import {  View,StyleSheet,Text,ScrollView,Dimensions  } from 'react-native';
+import * as Location from "expo-location";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Dimensions,
+  ActivityIndicator,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import { Fontisto } from "@expo/vector-icons";
 
-const {width} = Dimensions.get("window");
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
-const API_KEY = "7a471a2bac95bda3718d024d2d617361"
+const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
-  const [country,setCountry] = useState("Loading...")
-  const [days,setDays] = useState([]);
-  const [dd,setDd] = useState(true);
-  const getWeather = async() =>{
-   const {granted} = await Location.requestForegroundPermissionsAsync()
-    if(!granted){
-      setDd(false);
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
     }
-    const {coords:{latitude,longitude}} = await Location.getCurrentPositionAsync({accuracy:5})
-    const location = await Location.reverseGeocodeAsync({latitude,longitude}, {useGoogleMaps:false}
-      )
-     setCountry(location[0].country)
-   const response = await  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`)
-      const json = await response.json()
-      console.log(json)
-  }
-  useEffect(()=>{
-    getWeather(); 
-  },[])
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].country);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
-   <View style={styles.container}>
-    <View style={styles.city}>
-      <Text style={styles.cityName}>{country}</Text>
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="white"
+                />
+              </View>
+
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
-    <ScrollView 
-    pagingEnabled
-    horizontal 
-    contentContainerStyle={styles.weather}>
-      <View style={styles.day}>
-        <Text style={styles.temp}>22</Text>
-        <Text style={styles.des}>Sunny</Text>
-      </View>
-      <View style={styles.day}>
-        <Text style={styles.temp}>22</Text>
-        <Text style={styles.des}>Sunny</Text>
-      </View>
-      <View style={styles.day}>
-        <Text style={styles.temp}>22</Text>
-        <Text style={styles.des}>Sunny</Text>
-      </View>
-      <View style={styles.day}>
-        <Text style={styles.temp}>22</Text>
-        <Text style={styles.des}>Sunny</Text>
-      </View>
-      <View style={styles.day}>
-        <Text style={styles.temp}>22</Text>
-        <Text style={styles.des}>Sunny</Text>
-      </View>
-    </ScrollView>
-   </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"#8977ad",
+  container: {
+    flex: 1,
+    backgroundColor: "#7894ad",
   },
-  city:{
-    flex:1,
-    
-    justifyContent:"center",
-    alignItems:"center"
-
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  cityName:{
-    fontSize:48,
-    fontWeight:"600"
+  cityName: {
+    fontSize: 58,
+    fontWeight: "500",
+    color: "white",
   },
-  weather:{
-  
-   
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
   },
-  day:{ 
-    width:411,
-    alignItems:"center",
+  temp: {
+    marginTop: 50,
+    fontWeight: "600",
+    fontSize: 100,
+    color: "white",
   },
-  temp:{
-    marginTop:50,
-      fontSize: 158,
+  description: {
+    marginTop: -10,
+    fontSize: 30,
+    color: "white",
+    fontWeight: "500",
   },
-  des:{
-      fontSize:60,
-      marginTop:-30
-  }
-})
+  tinyText: {
+    marginTop: -5,
+    fontSize: 25,
+    color: "white",
+    fontWeight: "500",
+  },
+});
