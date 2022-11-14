7374c531cdcaa5fc7817baf08d5d089169b20a85
Jang Gyeoungguk <97073355+rnr9928@users.noreply.github.com>
2022-11-15 01:59:54 +0900

    Update App.js

--- 2do/App.js
+++ 2do/App.js
@@ -9,23 +9,50 @@ import {
   ScrollView,
   Alert,
 } from "react-native";
-
-
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from "./colors";
+import { Fontisto } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons'; 
+
 
 const STORAGE_KEY = "@toDos";
+const CATEGORY_KEY = "@category";
 
 export default function App() {
+ 
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+  const [newText, setNewText] = useState();
+
+  const travel = () => setWorking(false);
+  const  work = () => setWorking(true)
+
   useEffect(() => {
     loadToDos();
+    getCategory();
   }, []);
-  const travel = () => setWorking(false);
-  const work = () => setWorking(true);
+
+  const category = () =>{
+    saveCategory();
+  }
+
+
+  const saveCategory = async () => {
+    await AsyncStorage.setItem(CATEGORY_KEY, JSON.stringify(working))
+  }
+
+  const getCategory = async () => {
+    const prevCategory = await AsyncStorage.getItem(CATEGORY_KEY);
+    setWorking(JSON.parse(prevCategory))
+  }
+
+
+
   const onChangeText = (payload) => setText(payload);
+
+
   const saveToDos = async (toSave) => {
     await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
   };
@@ -66,28 +93,53 @@ export default function App() {
       },
     ]);
   };
+
+  const completeToDo = (key) =>{
+    const newToDos = {...toDos};
+    newToDos[key].complete = !newToDos[key].complete
+    setToDos(newToDos)
+    saveToDos(newToDos)
+  }
+
+  const updateToDo = (key) =>{
+    Alert.prompt('수정하기','수정할꺼임?',()=>{
+      let newToDos = {...toDos}
+      newToDos[key].text = newText
+      setToDos(newToDos);
+      saveToDos(newToDos);
+      setNewText("");
+    })
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity onPress={work}>
+        <TouchableOpacity onPress={category}>
+          <TouchableOpacity onPress={work}>
           <Text
-            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+            style={{  fontSize: 38,
+              fontWeight: "600", color: working ? "black" : theme.grey }}
           >
             Work
           </Text>
+          </TouchableOpacity>
         </TouchableOpacity>
+        <TouchableOpacity onPress={category}>
         <TouchableOpacity onPress={travel}>
           <Text
             style={{
-              ...styles.btnText,
-              color: !working ? "white" : theme.grey,
+              fontSize: 38,
+    fontWeight: "600",
+              color: !working ? "black" : theme.grey,
             }}
           >
             Travel
           </Text>
+          </TouchableOpacity>
         </TouchableOpacity>
       </View>
+      <View>
       <TextInput
         onSubmitEditing={addToDo}
         onChangeText={onChangeText}
@@ -98,17 +150,39 @@ export default function App() {
         }
         style={styles.input}
       />
+      </View>
       <ScrollView>
-        {Object.keys(toDos).map((key) =>
+        {toDos ? (  
+        Object.keys(toDos).map((key) =>
           toDos[key].working === working ? (
             <View style={styles.toDo} key={key}>
-              <Text style={styles.toDoText}>{toDos[key].text}</Text>
-              <TouchableOpacity onPress={()=>deleteToDo(key)}>
-              <Text>❌</Text>
+              <Text style={toDos[key].complete ? ({...styles.toDoText,
+              textDecorationLine: 'line-through'}
+              ):(
+                styles.toDoText
+              )}>{toDos[key].text}
+              </Text>
+              <View style={{justifyContent: 'flex-end', flexDirection: 'row'}}>
+              <TouchableOpacity style={{marginRight:12 , marginTop:-5}} onPress={()=>deleteToDo(key)}>
+              <MaterialCommunityIcons name="delete" size={28} color="black" />
               </TouchableOpacity>
+             
+                    <TouchableOpacity style={{marginRight:15 , marginTop:-3.5}} onPress={()=>updateToDo(key)}>
+                    <Ionicons name="md-newspaper" size={24} color="black" />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{marginRight:15 , marginTop:0}} onPress={() => completeToDo(key)}>
+											<Fontisto
+												name={toDos[key].complete ? 'checkbox-active' : 'checkbox-passive'}
+												size={18}
+												color="white"
+											/>
+										</TouchableOpacity>
+            </View>
             </View>
           ) : null
-        )}
+        )
+      ) :null}
       </ScrollView>
     </View>
   );
@@ -129,12 +203,13 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
   input: {
-    backgroundColor: "white",
+    backgroundColor: "#A3B6C5",
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
     marginVertical: 20,
     fontSize: 18,
+  
   },
   toDo: {
     backgroundColor: theme.toDoBg,
@@ -142,12 +217,14 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 30,
     borderRadius: 15,
+    marginRight: 10,
+    justifyContent: "space-between",
     flexDirection:"row",
     alignItems:"center",
-    justifyContent: "space-between",
+   
   },
   toDoText: {
-    color: "white",
+    color: "#292f36",
     fontSize: 30,
     fontWeight: "600",
   },
